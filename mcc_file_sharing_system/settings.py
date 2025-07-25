f3066ccf25a9dde4ca268f0873ed56207aba9768
MUGABI-PRAISE <97666544+MUGABI-PRAISE@users.noreply.github.com>
2025-07-25 18:04:22 +0000

    Update settings.py

--- mcc_file_sharing_system/settings.py
+++ mcc_file_sharing_system/settings.py
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # allow other domains to login
 CORS_ALLOWED_ORIGINS = [
-    "https://mccfilesharingsystem-neon.vercel.app",
+    "https://mccfilesharingsystem.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://10.42.0.1:3000",
