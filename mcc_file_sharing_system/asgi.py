b47ddfc248e45e428b497f8085b2d28176efff7e
mugabi praise <mugabipraise4@gmail.com>
2025-09-06 21:21:23 +0300

    fixed missing contenttypes

--- mcc_file_sharing_system/asgi.py
+++ mcc_file_sharing_system/asgi.py
@@ -12,12 +12,14 @@ it has now been wrapped by daphne, an asgi server for websockets.
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mcc_file_sharing_system.settings")
+
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mcc_file_sharing_system.settings.development")
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
