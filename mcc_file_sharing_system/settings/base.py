74191db7a50e28db2e484468e10ad9bf78ef039e
mugabi praise <mugabipraise4@gmail.com>
2025-09-11 01:44:18 +0300

    fixed the error

--- mcc_file_sharing_system/settings/base.py
+++ mcc_file_sharing_system/settings/base.py
@@ -179,7 +179,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ),mccfilesharingsystem-neon.vercel.app
+    ),
 }
 
 # ---------------------------------------------------------------------
